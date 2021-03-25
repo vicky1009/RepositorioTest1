fc80cf2029e9a6b9d242e3bd34544ab1679f6359
vicky1009 <vickycadorin@hotmail.com>
2021-03-25 15:59:06 +0100

    Cambio al uso del modulo datos

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import utilidades as ut
-
-def print_hi(name):
-    print(f'Hi, {name}')
+import datos
 
 
 if __name__ == '__main__':
-    print_hi('Hola mundo!!!')
-    ut.saludo()
+    print( 'Hola soy, ', datos.nombre)
